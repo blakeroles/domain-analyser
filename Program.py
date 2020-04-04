21aedc73ee57b134dcfc91f928d4fe864fe19c67
Blake Roles <blakeroles@gmail.com>
2020-04-05 10:41:08 +1100

    Get api info from file

--- Program.py
+++ Program.py
@@ -1,5 +1,6 @@
 from Suburb import Suburb
 from Property import Property
+from DomainCredentials import DomainCredentials
 import json
 import requests
 
@@ -8,11 +9,18 @@ PROPERTY_TYPES = ["Townhouse", "House"]
 DOMAIN_CREDENTIALS_FILENAME = "api_info.secret"
 
 def get_domain_credentials():
-	
+	with open(DOMAIN_CREDENTIALS_FILENAME) as f:
+		data = json.load(f)
 
+	dc = DomainCredentials(data["api_info"]["client_id"], data["api_info"]["client_secret"])
 
+	return dc
 
 def main():
+
+	# Get your domain credentials
+	dc = get_domain_credentials()
+
 
 	suburb_obj_list = []
 
